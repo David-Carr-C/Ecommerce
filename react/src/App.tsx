31f0c39be921b27ef53a001cc7d374a83bbded52
david-carrillo <david-carrillo@debian-BULLSEYE-live-builder-AMD64>
2023-12-06 17:08:44 -0600

    NextUI added

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material';
 import './App.css'
 import { useState } from 'react';
+import { Checkbox } from '@nextui-org/checkbox';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,14 +9,26 @@ function App() {
   return (
     <>
       <p>Hello World!</p>
-      <Button variant="contained" onClick={() => setCount(count + 1)}>
+      <Button variant="contained" onClick={() => setCount(count + 1)} sx={buttonClass}>
         Hello World {count}
       </Button>
       <div className="text-center">
         <img src="https://picsum.photos/200/300" alt="random" className='rounded-full inline-block' />
       </div>
+      Checkbox from NextUI
+      <Checkbox defaultChecked color='danger' className='m-2 uppercase'>
+        Checkbox
+      </Checkbox>
     </>
   );
 }
 
 export default App
+
+const buttonClass = {
+  backgroundColor: 'white',
+  color: 'gray',
+  '&:hover': {
+    backgroundColor: 'gray'
+  }
+}
--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.tsx'],
+  content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.tsx', 
+  './node_modules/@nextui-org/theme/dist/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
+  darkMode: 'class',
   plugins: [],
 }
 
